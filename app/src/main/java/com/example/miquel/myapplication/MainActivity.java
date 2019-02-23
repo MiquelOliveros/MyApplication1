d2880ec91cb7bd1eb33b6d12ee950968bfe9332a
ar98nau <anr3@alumnes.udl.cat>
2019-02-23 11:20:57 +0100

    Imatges i Toast: FINALITZADA

--- app/src/main/java/com/example/miquel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/miquel/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.miquel.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button clickButton = findViewById(R.id.button);
+        clickButton.setOnClickListener(new Toaster());
+    }
+
+    private class Toaster implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, R.string.toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
